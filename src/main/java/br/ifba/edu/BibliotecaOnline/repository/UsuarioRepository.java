6b1c4f2c5afe79d1bce1b66b4c5d89de1cdb90d7
LuizHFAnjos <l.henriquetrabalho@gmail.com>
2025-09-22 12:04:05 -0300

    Update UsuarioRepository

--- src/main/java/br/ifba/edu/BibliotecaOnline/repository/UsuarioRepository.java
+++ src/main/java/br/ifba/edu/BibliotecaOnline/repository/UsuarioRepository.java
@@ -1,6 +1,9 @@
 package br.ifba.edu.BibliotecaOnline.repository;
 
 import br.ifba.edu.BibliotecaOnline.entities.Usuario;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -11,4 +14,7 @@ public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
     Optional<Usuario> findByEmail(String email);
 
     boolean existsByEmail(String email);
+
+    Page<Usuario> findByNomeContainingIgnoreCaseOrEmailContainingIgnoreCase(String palavraChave, String palavraChave2,
+            Pageable pageable);
 }
