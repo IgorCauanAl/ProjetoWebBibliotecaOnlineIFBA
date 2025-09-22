a513f7a64707f703e3d4d1c2eaca6b1bcd37f479
Guilherme Henrique <eu.guilherme.henriq@gmail.com>
2025-09-22 19:36:53 -0300

    funcionalidade/Tratamento global de exceções com ControllerAdvice

--- src/main/java/br/ifba/edu/BibliotecaOnline/excecao/LoginIncorretoException.java
+++ src/main/java/br/ifba/edu/BibliotecaOnline/excecao/LoginIncorretoException.java
@@ -1,6 +1,10 @@
 package br.ifba.edu.BibliotecaOnline.excecao;
 
-public class LoginIncorretoException extends RuntimeException{
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class LoginIncorretoException extends RuntimeException {
 
     public LoginIncorretoException(){
         super("Login ou senha incorreto!");
